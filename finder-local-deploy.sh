09df48abe080d92d947b56bdea78047f32950414
mikebychkov <mikebychkov@github.com>
2021-02-20 03:05:33 +0500

    working on kafka deployment

--- finder-local-deploy.sh
+++ finder-local-deploy.sh
@@ -1,5 +1,7 @@
-export IMG_TAG=$(date -u "+%Y%m%d%H%M%S")
 eval $(minikube docker-env)
+export IMG_TAG=latest
+mvn -f ../finder/ clean package docker:build
+export IMG_TAG=$(date -u "+%Y%m%d%H%M%S")
 mvn -f ../finder/ clean package docker:build
 kubectl set image deployments/finder-deployment finder=mikebychkov/sb-finder:$IMG_TAG
 kubectl get pods
--- posts-local-deploy.sh
+++ posts-local-deploy.sh
@@ -1,5 +1,7 @@
-export IMG_TAG=$(date -u "+%Y%m%d%H%M%S")
 eval $(minikube docker-env)
-mvn -f ../finder/ clean package docker:build
-kubectl set image deployments/finder-deployment finder=mikebychkov/sb-finder:$IMG_TAG
+export IMG_TAG=latest
+mvn -f ../posts/ clean package docker:build
+export IMG_TAG=$(date -u "+%Y%m%d%H%M%S")
+mvn -f ../posts/ clean package docker:build
+kubectl set image deployments/posts-deployment posts=mikebychkov/sb-posts:$IMG_TAG
 kubectl get pods
